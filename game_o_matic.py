465685221d7b774169d4db8919998cba101bb43e
Tyler S <tylerseliber@gmail.com>
2021-04-18 22:56:49 -0400

    Started work on players and inventory

--- game_o_matic.py
+++ game_o_matic.py
@@ -1,13 +1,33 @@
 import random
 
-rand_numbers = []
-dodecahedron_results = [0] * 12
+# Part 1
 
-print(dodecahedron_results)
+# rand_numbers = []
+# dodecahedron_results = [0] * 12
 
-for i in range(0,10000):
-    r = random.randint(1,12)
-    rand_numbers.append(r)
-    dodecahedron_results[r-1] += 1
+# for i in range(0,10000):
+#     r = random.randint(1,12)
+#     rand_numbers.append(r)
+#     dodecahedron_results[r-1] += 1
 
-print(dodecahedron_results)
+# print(dodecahedron_results)
+
+# Part 2
+
+def printPlayerInventories(player_list):
+    for p in player_list.keys():
+        print(p, end=':\n')
+        for i in player_list[p].items():
+            print(f'\t{i[1]} {i[0]}')
+        # print(p.values())
+        # for i in p:
+        #     print(i)
+
+players = dict()
+players['Gandolf'] = {'food': 5, 'grapefruit': 10, 'green potions': 7, 'red potions': 8, 'spells of enchantment': 10}
+players['Frodo'] = {'food': 0, 'kiwi': 5, 'wands of confusion': 7, 'green potions': 8}
+players['Sauron'] = {'bat wings': 5, 'evil spells': 10, 'fire wands': 5}
+
+printPlayerInventories(players)
+# print(players.items())
+
