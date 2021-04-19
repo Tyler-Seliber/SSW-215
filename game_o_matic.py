ad5e94fa01fc06db4ae57506cc6ed2e027815239
Tyler S <tylerseliber@gmail.com>
2021-04-18 23:50:43 -0400

    Finished part 2

--- game_o_matic.py
+++ game_o_matic.py
@@ -23,7 +23,7 @@ def removePlayer(player_list, name):
     print(f'So long, {name}.')
 
 def printPlayerInventories(player_list):
-    print('Current player invntories:')
+    print('Current player inventories:')
     for p in player_list.keys():
         print('\t' + p + ':')
         for i in player_list[p].items():
@@ -35,7 +35,8 @@ def addToInventory(player_list, player, item, amount):
     print(f'{player} has received {amount} {item}.')
 
 def removeFromInventory(player_list, player, item, amount):
-    if (player_list[player][item] - amount) < 0:
+    # Prevent having negative amount of items, or quickly remove all items
+    if ((player_list[player][item] - amount) < 0) or (amount == -1):
         amount = player_list[player][item]
         # Removes the item from the inventory if the amount is zero
         player_list[player].pop(item)
@@ -44,15 +45,19 @@ def removeFromInventory(player_list, player, item, amount):
     print(f'{player} has lost {amount} {item}.')
 
 players = dict()
+
 addPlayer(players, 'Gandolf', {'food': 5, 'grapefruit': 10, 'green potions': 7, 'red potions': 8, 'spells of enchantment': 10})
 addPlayer(players, 'Frodo', {'food': 0, 'kiwi': 5, 'wands of confusion': 7, 'green potions': 8})
 addPlayer(players, 'Sauron', {'bat wings': 5, 'evil spells': 10, 'fire wands': 5})
 
 printPlayerInventories(players)
 
-removeFromInventory(players, 'Gandolf', 'food', 100)
-printPlayerInventories(players)
-
+# Test the three capabilities required by the assignment
+addToInventory(players, 'Gandolf', 'food', 18)
+removeFromInventory(players, 'Gandolf', 'green potions', -1)
+addToInventory(players, 'Sauron', 'spider eyes', 4)
+removeFromInventory(players, 'Sauron', 'bat wings', 3)
 removePlayer(players, 'Frodo')
-printPlayerInventories(players)
+addPlayer(players, 'Windelschleppitis', players['Gandolf'].copy())
 
+printPlayerInventories(players)
