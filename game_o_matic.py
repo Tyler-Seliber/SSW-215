6c6de7a15d39ea798b689c3dfe2b7611be4fa874
Tyler S <tylerseliber@gmail.com>
2021-04-18 23:34:40 -0400

    Added removePlayer(), adjusted formatting

--- game_o_matic.py
+++ game_o_matic.py
@@ -14,14 +14,20 @@ import random
 
 # Part 2
 
-def addPlayers(player_list, name, inventory):
+def addPlayer(player_list, name, inventory):
     player_list[name] = inventory
+    print(f'Welcome, {name}.')
+
+def removePlayer(player_list, name):
+    player_list.pop(name)
+    print(f'So long, {name}.')
 
 def printPlayerInventories(player_list):
+    print('Current player invntories:')
     for p in player_list.keys():
-        print(p + ':')
+        print('\t' + p + ':')
         for i in player_list[p].items():
-            print(f'\t{i[1]} {i[0]}')
+            print(f'\t\t{i[1]} {i[0]}')
 
 def addToInventory(player_list, player, item, amount):
     # Adds amount to item in player's inventory. If item does not exist, add it to player's inventory and set the quantity to amount.
@@ -38,12 +44,15 @@ def removeFromInventory(player_list, player, item, amount):
     print(f'{player} has lost {amount} {item}.')
 
 players = dict()
-addPlayers(players, 'Gandolf', {'food': 5, 'grapefruit': 10, 'green potions': 7, 'red potions': 8, 'spells of enchantment': 10})
-addPlayers(players, 'Frodo', {'food': 0, 'kiwi': 5, 'wands of confusion': 7, 'green potions': 8})
-addPlayers(players, 'Sauron', {'bat wings': 5, 'evil spells': 10, 'fire wands': 5})
+addPlayer(players, 'Gandolf', {'food': 5, 'grapefruit': 10, 'green potions': 7, 'red potions': 8, 'spells of enchantment': 10})
+addPlayer(players, 'Frodo', {'food': 0, 'kiwi': 5, 'wands of confusion': 7, 'green potions': 8})
+addPlayer(players, 'Sauron', {'bat wings': 5, 'evil spells': 10, 'fire wands': 5})
 
 printPlayerInventories(players)
 
 removeFromInventory(players, 'Gandolf', 'food', 100)
+printPlayerInventories(players)
+
+removePlayer(players, 'Frodo')
 printPlayerInventories(players)
 
