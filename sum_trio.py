7af4d35a48d528c7416342271c66a7233f1029d2
Tyler S <tylerseliber@gmail.com>
2021-04-26 19:32:14 -0400

    finished sum_trio

--- sum_trio.py
+++ sum_trio.py
@@ -1,18 +1,17 @@
 # Part 1
 
 def sum_trio(numSet, sum, list = []):
-    # trio = []
-    # intermediate_sum = 0
-    if len(list) == 3:
-        return list
-    x = numSet[1]
-    list.append(x)
-    if sum - x == numSet[1]:
-        list.append(sum_trio(numSet[1:], numSet[1], list))
-    else:
-        return list
-        
+    # print(list)
+    if len(list) == 3 and sum == 0: #Trio found
+        return [list]
+    
+    if not numSet or len(list) == 3: #Haven't found trio or 3 numbers aren't a trio, return nothing (start over)
+        return []
+
+    return sum_trio(numSet[1:], sum - numSet[0], [numSet[0]]+list) + sum_trio(numSet[1:], sum, list)
+
 
 numSet = [3, 7, 4, 0, 9, 5, 1, 2]
 sum = 7
-print(sum_trio(numSet, sum))
+
+print(sorted(list(map(sorted, sum_trio(numSet, sum)))))
