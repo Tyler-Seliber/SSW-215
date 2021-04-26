329aca6e783828d753b761ac1818209aa3a3389d
Tyler S <tylerseliber@gmail.com>
2021-04-26 16:57:35 -0400

    Working on it

--- sum_sequence.py
+++ sum_sequence.py
@@ -5,18 +5,9 @@ def sum_sequence(sum, start = 1):
     arr = [1] * sum
     if arr[0] != sum:
         print(arr)
-        return sum_sequence(sum - 1) + ' '
+        return (str(arr[0]+arr[1]) + ' '+ sum_sequence(sum - 1) + ' ')
     else:
-        return '\n'
-    print(arr)
-
-    # if sum != 1:
-    #     return str(start) + ' ' + str(sum_sequence(sum - 1))
-    # elif start != sum:
-    #     return '\n' + sum_sequence(sum, start + 1)
-    # else:
-    #     return ''
-    
+        return '\n'  
 
 k = 4
 print(sum_sequence(k))
--- sum_trio.py
+++ sum_trio.py
@@ -1,12 +1,18 @@
 # Part 1
 
-def sum_trio(numSet, sum, index = 0):
-    trio = []
-    intermediate_sum = 0
-    for i in range(numSet):
-        pass
+def sum_trio(numSet, sum, list = []):
+    # trio = []
+    # intermediate_sum = 0
+    if len(list) == 3:
+        return list
+    x = numSet[1]
+    list.append(x)
+    if sum - x == numSet[1]:
+        list.append(sum_trio(numSet[1:], numSet[1], list))
+    else:
+        return list
         
 
 numSet = [3, 7, 4, 0, 9, 5, 1, 2]
 sum = 7
-sum_trio(numSet, sum)
+print(sum_trio(numSet, sum))
